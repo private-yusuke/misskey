345d99f1dbf80d9a6f247f28d86b2c00b3a2019f
takonomura <takonomura@users.noreply.github.com>
2020-09-21 23:24:37 +0900

    全チャンネルのリストを見れるように

--- src/server/api/endpoints/channels/featured.ts
+++ src/server/api/endpoints/channels/featured.ts
@@ -1,3 +1,4 @@
+import $ from 'cafy';
 import define from '../../define';
 import { Channels } from '../../../../models';
 
@@ -5,6 +6,21 @@ export const meta = {
 	tags: ['channels'],
 
 	requireCredential: false as const,
+
+	params: {
+		limit: {
+			validator: $.optional.num.range(1, 100),
+			default: 10,
+			desc: {
+				'ja-JP': '最大数'
+			}
+		},
+
+		offset: {
+			validator: $.optional.num.min(0),
+			default: 0
+		},
+	},
 
 	res: {
 		type: 'array' as const,
@@ -22,7 +38,7 @@ export default define(meta, async (ps, me) => {
 		.where('channel.lastNotedAt IS NOT NULL')
 		.orderBy('channel.lastNotedAt', 'DESC');
 
-	const channels = await query.take(10).getMany();
+	const channels = await query.skip(ps.offset).take(ps.limit).getMany();
 
 	return await Promise.all(channels.map(x => Channels.pack(x, me)));
 });
